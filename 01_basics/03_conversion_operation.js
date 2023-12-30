f2ba869421220d67c3205f987c3af5415950222e
suryansh0010 <121747123+suryansh0010@users.noreply.github.com>
2023-12-30 21:14:07 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,24 @@
+// let score="99abs"
+
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false =>0
+
+// let isLoggedIn = "harhsit"
+// let booleanIsLoggedIn = Boolean(isLoggedIn) 
+// console.log(booleanIsLoggedIn)
+
+
+//1 => true
+//"" =>false
+//"harshit" => true
+
+let someNumber =12
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
