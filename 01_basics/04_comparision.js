e1db411b9cf02ec26ca45b9dc47abb22c2683dcd
suryansh0010 <121747123+suryansh0010@users.noreply.github.com>
2023-12-30 21:54:12 +0000

    comarisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,10 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// === check the value as well data type
+// == only check value
