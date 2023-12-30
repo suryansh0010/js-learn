30c41a133e19451ff0a78acf2032e777321487a2
suryansh0010 <121747123+suryansh0010@users.noreply.github.com>
2023-12-30 22:14:31 +0000

    datatype_summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,27 @@
+// Premitive
+
+// 7 Types : String, Number, Boolean, null,undefiend, symbol
+// ,BigInt
+
+const score=100
+const scoreValue =100.3
+const isLoggedIn =  false
+let userEmail;
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id === anotherid);
+
+//Reference (Non primitive)
+
+//Array, object ,Functions
+
+const heros =["shaktiman","naagraj","harshit"]
+let myObject ={
+    name:"hitesh",
+    age:12,
+    Gender:"M"
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
