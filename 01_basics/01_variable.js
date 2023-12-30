0d5a00b1bb813201e3eb646fe32b7dd3754af650
suryansh0010 <121747123+suryansh0010@users.noreply.github.com>
2023-12-30 20:41:48 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const userId = 12334
+let Gmail ="harshit.939@gmail"
+var accountPass="hfda8r9"
+age = 12
+age =14
+
+let accountState;
+// console.log(userId);
+// console.log(Gmail);
+// console.log(accountPass);
+// console.log(age);
+
+console.table([userId,Gmail,accountPass,age,accountState])
+
+/*
+prefer not to use var
+beacuse of issue in functional scope and block scope
+*/
---
+++ 01_basics/02_data_type.js
@@ -0,0 +1,23 @@
+
+"use strict"// treat all JS code as newer version
+
+// alert("hello") // we are using node js not browser
+console.log("harsh");
+
+let name = "harsh"
+let age = 20
+let isLoggedIn = false
+
+//number 
+//bigint
+//boolean=true/false
+//string=""
+//null
+//undefined
+//symbol=unique
+
+
+//object
+
+console.log(typeof(null))  // object
+console.log(typeof undefined)  //undefiend
