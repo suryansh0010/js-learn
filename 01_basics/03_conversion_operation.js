ba7165da1d823e454dabe2e1cd37148df052b8ec
suryansh0010 <121747123+suryansh0010@users.noreply.github.com>
2023-12-30 21:39:35 +0000

    Conversion od datatype

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -18,7 +18,35 @@
 //"" =>false
 //"harshit" => true
 
-let someNumber =12
-let stringNumber = String(someNumber)
+// let someNumber =12
+// let stringNumber = String(someNumber)
 
-console.log(stringNumber);
+// console.log(stringNumber);
+
+
+// ******************* OPERATIONS ****************
+
+// let value = 3
+// let negvalue = -value //if you use ~ this sign instead of of - this you got -4 output because ~ give 2's complement of the given number
+
+// console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 ="hello"
+// let str2 ="hitesh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" +2 +3);
+// console.log(1 +2 + "3"); 
+
+// let gameCounter = 100
+// gameCounter++;    // for increamenter
+// console.log(gameCounter);
