ff9317129ff7f6fa1af6a114465916a01ff3a84f
suryansh0010 <121747123+suryansh0010@users.noreply.github.com>
2024-01-12 19:46:32 +0000

    Arrow in js

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,57 @@
+const user= {
+    username : "hitesh",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username},welcome to website`);
+        console.log(this)
+    }
+
+}
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this)
+
+// browser k andar global object "window" hota hai
+
+
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+
+// const chai = function(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+const chai = () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+// chai()
+
+// const addTwo= (num1,num2) => {
+//     return num1+num2
+// }
+
+
+// const addTwo= (num1,num2) => num1+num2
+// const addTwo= (num1,num2) => (num1 + num2)
+
+const addTwo= (num1,num2) => ({username:"harshit"})
+
+
+console.log(addTwo(3,4))
+
+const myArray =[2,3,4,5,7]
+myArray.forEach(() => {})
