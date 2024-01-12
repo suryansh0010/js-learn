1a3c512c6ba8130b053e3c211a92182014f66435
suryansh0010 <121747123+suryansh0010@users.noreply.github.com>
2024-01-12 19:50:32 +0000

    if else in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,23 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+const UserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(UserloggedIn && debitCard && (loggedInFromGoogle || loggedInFromEmail)){
+    console.log("Allow to buy course");
+}
+
+  
