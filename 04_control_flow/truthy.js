e0da12a6995462ee1c5cbba91470ea9984b05414
suryansh0010 <121747123+suryansh0010@users.noreply.github.com>
2024-01-13 17:11:36 +0000

    truthy and falsy in js

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,29 @@
+const userEmail = "harhsit@google.com"
+
+if(userEmail){
+    console.log("Got user email");
+}else{
+    console.log("Don't have user email");
+}
+
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+
+
+// if(userEmail.length === 0){
+//     connsole.log("Array is empty");
+// }
+
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("Object is empty");
+} 
